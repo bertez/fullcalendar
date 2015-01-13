1093c2faa475059c5546159f29ef24d9b9b06830
Berto Yáñez <bertez@gmail.com>
2015-01-13 10:49:51 +0100

    Added Galician language

---
+++ lang/gl.js
@@ -0,0 +1,11 @@
+
+$.fullCalendar.lang("es", {
+	defaultButtonText: {
+		month: "Mes",
+		week: "Semana",
+		day: "Día",
+		list: "Axenda"
+	},
+	allDayHtml: "Todo<br/>o día",
+	eventLimitText: "máis"
+});
